e8acc03c75edc82c801597d838cc7b6f9e0fcf60
Sajjad <sajjad.saberi1996@gmail.com>
2023-11-14 22:08:33 +0330

    add question and answers in this app

--- js/game.js
+++ js/game.js
@@ -2,21 +2,36 @@ import formatData from "./helper.js";
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container") ;
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
+
+
 const URL = "https://opentdb.com/api.php?amount=10&type=multiple";
 
 let formattedData = null ;
+let questionIndex = 0 ;
+let correctAnswer = null ;
 
 const fetchData = async () => {
     const response = await fetch(URL) ;
     const json = await response.json();
     formattedData = formatData(json.results) ;
-    console.log(formattedData);
     start();
 };
 
 const start = () => {
+    showQuestion();
     loader.style.display = "none" ;
     container.style.display = "block";
+};
+
+const showQuestion = () => {
+    const {question, answers, correctAnswerIndex} = formattedData[questionIndex];
+    correctAnswer = correctAnswerIndex ;
+    questionText.innerText = question ;
+    answerList.forEach((button, index) => {
+        button.innerText = answers[index] ;
+    });
 };
 
 window.addEventListener("load", fetchData) ;
