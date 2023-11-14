12bb038ccacffb6ceaaa94c3e37518c751bbef69
Sajjad <sajjad.saberi1996@gmail.com>
2023-11-14 21:53:20 +0330

    get data in unique module

--- js/game.js
+++ js/game.js
@@ -1,23 +1,16 @@
+import formatData from "./helper.js";
+
 const loader = document.getElementById("loader");
 const container = document.getElementById("container") ;
 const URL = "https://opentdb.com/api.php?amount=10&type=multiple";
 
 let formattedData = null ;
 
-const formatData = (questionData) => {
-    const result = questionData.map(item => {
-        const questionObject = {question: item.question};
-        const answers = [...item.incorrect_answers];
-        const correctAnswerIndex = Math.floor(Math.random() * 4);
-    })
-}
-
-
 const fetchData = async () => {
     const response = await fetch(URL) ;
     const json = await response.json();
-    // formattedData = json ;
-    formatData(json.results) ;
+    formattedData = formatData(json.results) ;
+    console.log(formattedData);
     start();
 };
 
