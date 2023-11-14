05e64aac3ef5956ce9e0c7e5e9c95a3d79d4dc5f
Sajjad <sajjad.saberi1996@gmail.com>
2023-11-14 22:28:51 +0330

    show the correct and incorrect answers in app

--- js/game.js
+++ js/game.js
@@ -34,7 +34,21 @@ const showQuestion = () => {
     });
 };
 
+const checkAnswer = (event, index) => {
+    const isCorrect = index === correctAnswer ? true : false ;
+    if(isCorrect) {
+        event.target.classList.add("correct") ;
+    } else {
+        event.target.classList.add("incorrect") ;
+        answerList[correctAnswer].classList.add("correct") ;
+    };
+};
+
 window.addEventListener("load", fetchData) ;
+answerList.forEach((button, index) => {
+    const Handler = (event) => {checkAnswer(event, index)}
+    button.addEventListener("click", Handler) ;
+});
 
 
 
