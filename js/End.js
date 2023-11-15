575d95422efc7455e8870cfb59e0bb7613fa03a9
Sajjad <sajjad.saberi1996@gmail.com>
2023-11-15 23:46:30 +0330

    add scores and highScores option in APP

--- js/End.js
+++ js/End.js
@@ -1,4 +1,6 @@
 const score = JSON.parse(localStorage.getItem("score"));
+const highScores = JSON.parse(localStorage.getItem("highScores"));
+
 const scoreElement = document.querySelector("p");
 const button = document.querySelector("button");
 const input = document.querySelector("input");
@@ -6,11 +8,18 @@ const input = document.querySelector("input");
 
 scoreElement.innerText = score ;
 
+
 const saveHandler = () => {
     if(input.value || !score) {
         alert("Invalid Username Or Score!")
     } else {
-        const finalScore = {name: input.value, score}
+        const finalScore = {name: input.value, score};
+        highScores.push(finalScore);
+        highScores.sort((a,b) => b.score - a.score) ;
+        highScores.splice(10) ;
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+        localStorage.removeItem("scores") ;
+        window.location.assign("/") ;
     };
 };
 
