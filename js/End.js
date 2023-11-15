8071cc0af1eaba02ee59eedd3a88e078963d824d
Sajjad <sajjad.saberi1996@gmail.com>
2023-11-15 23:38:45 +0330

    add high scores in app

--- js/End.js
+++ js/End.js
@@ -1,6 +1,18 @@
-const Hello = () => {
-    console.log("Hello World!");
+const score = JSON.parse(localStorage.getItem("score"));
+const scoreElement = document.querySelector("p");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+
+
+scoreElement.innerText = score ;
+
+const saveHandler = () => {
+    if(input.value || !score) {
+        alert("Invalid Username Or Score!")
+    } else {
+        const finalScore = {name: input.value, score}
+    };
 };
 
 
-export default Hello ;
+button.addEventListener("click", saveHandler)
